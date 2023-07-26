f9eb50dfb933d3416c925e4a61c9b05f5d105fd4
Devasy23 <20bce057@nirmauni.ac.in>
2023-07-26 18:43:11 +0530

    did some aesthetic changes

--- app.py
+++ app.py
@@ -87,48 +87,54 @@ def app():
     st.title('Image Captioning')
     
     # Allow the user to upload an image
-    uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
+    col1, col2 = st.columns([0.7, 0.3], gap='large')
+    with col1:
+        uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, width=300, caption='Uploaded Image' )
+        with col2:
+            st.image(image, width=300, caption='Uploaded Image' )
         
         
         # give an option to choose between 2 models
-        model = st.selectbox('Choose a model', ['Model 1', 'Model 2'])
+        with col1:
+            model = st.selectbox('Choose a model', ['Model 1', 'Model 2'])
         if model == 'Model 1':
             st.info('Chosen model : ResNet50 model-20 epochs')
         
-        
-        # Generate and display the caption
-            x= preprocess_image(uploaded_file)
-            x = x.reshape(1, -1)
-            tokenizer = pickle.load(open('tokenizer_best.pkl', 'rb'))
-            loaded_Model = tf.keras.models.load_model('best_model.h5', compile=False)
-            loaded_Model.compile(loss='categorical_crossentropy', optimizer='adam')
-             
-            index_word = dict([(index, word)
-                for word, index in tokenizer.word_index.items()]) 
-            caption = predict_caption(x, tokenizer, loaded_Model, 35, index_word)
-            # st.header('Caption:')
-            caption = format_caption(caption)
-            st.header('Caption : '+ caption)
-            # st.write(caption)
+            with st.spinner('Predicting Output...'):
+                
+            # Generate and display the caption
+                x= preprocess_image(uploaded_file)
+                x = x.reshape(1, -1)
+                tokenizer = pickle.load(open('tokenizer_best.pkl', 'rb'))
+                loaded_Model = tf.keras.models.load_model('best_model.h5', compile=False)
+                loaded_Model.compile(loss='categorical_crossentropy', optimizer='adam')
+                
+                index_word = dict([(index, word)
+                    for word, index in tokenizer.word_index.items()]) 
+                caption = predict_caption(x, tokenizer, loaded_Model, 35, index_word)
+                # st.header('Caption:')
+                caption = format_caption(caption)
+                st.header('Caption : '+ caption)
+                # st.write(caption)
             
         elif model == 'Model 2':
-            loaded_Model = tf.keras.models.load_model('my_model.h5', compile=False)
-            loaded_Model.compile(loss='categorical_crossentropy', optimizer='adam')
-            
-            tokenizer = pickle.load(open('tokenizer.pkl', 'rb'))
-            index_word = dict([(index, word)
-                for word, index in tokenizer.word_index.items()])
-            
-            st.info('Chosen Model : VGG16 model-20 epochs')
-            x= preprocess_image(uploaded_file)
-            x = x.reshape(1, -1)
-            caption = predict_caption(x, tokenizer, loaded_Model, 30, index_word)
-            caption = format_caption(caption)
-            st.header('Caption : '+ caption)
-            # st.write(caption)
+            with st.spinner('Predicting Output...'):
+                loaded_Model = tf.keras.models.load_model('my_model.h5', compile=False)
+                loaded_Model.compile(loss='categorical_crossentropy', optimizer='adam')
+                
+                tokenizer = pickle.load(open('tokenizer.pkl', 'rb'))
+                index_word = dict([(index, word)
+                    for word, index in tokenizer.word_index.items()])
+                
+                st.info('Chosen Model : VGG16 model-20 epochs')
+                x= preprocess_image(uploaded_file)
+                x = x.reshape(1, -1)
+                caption = predict_caption(x, tokenizer, loaded_Model, 30, index_word)
+                caption = format_caption(caption)
+                st.header('Caption : '+ caption)
+                # st.write(caption)
 
     
 # import streamlit as st
